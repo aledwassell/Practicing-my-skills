e35031ac8b2cc7d47b50ca55ae1ee364ccf80276
aledwassell <aledwassell@googlemail.com>
2016-06-30 08:58:35 +0900

    cleaned up the code

--- js/main.js
+++ js/main.js
@@ -21,23 +21,38 @@ var todoList = {
     var completedTodos = 0;
 
     // Get number of completed todos.
-    for (var i = 0; i < totalTodos; i++) {
-      if (this.todos[i].completed === true) {
+    // for (var i = 0; i < totalTodos; i++) {
+    //   if (this.todos[i].completed === true) {
+    //     completedTodos++;
+    //   }
+    // }
+
+    this.todos.forEach(function(todo) {
+      if (todo.completed === true) {
         completedTodos++;
       }
-    }
+    })
 
-    // Case 1: If everything’s true, make everything false.
-    if (completedTodos === totalTodos) {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = false;
+    // // Case 1: If everything’s true, make everything false.
+    // if (completedTodos === totalTodos) {
+    //   for (var i = 0; i < totalTodos; i++) {
+    //     this.todos[i].completed = false;
+    //   }
+    // // Case 2: Otherwise, make everything true.
+    // } else {
+    //   for (var i = 0; i < totalTodos; i++) {
+    //     this.todos[i].completed = true;
+    //   }
+    // }
+    this.todos.forEach(function(todo) {
+      //if everything is true make everything false
+      if (completedTodos === totalTodos) {
+        todo.completed = false;
+      //else make everything true
+      } else {
+        todo.completed = true;
       }
-    // Case 2: Otherwise, make everything true.
-    } else {
-      for (var i = 0; i < totalTodos; i++) {
-        this.todos[i].completed = true;
-      }
-    }
+    });
   }
 };
 
