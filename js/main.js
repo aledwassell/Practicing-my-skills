2c8fe610814e00bdc0ac9777579b3c94d9a43261
aledwassell <aledwassell@googlemail.com>
2016-06-30 07:16:10 +0900

    Escape from the console

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
 var todoList = {
   todos: [],
-  // displayTodos: function() {
-  //   if (this.todos.length === 0){
-  //     console.log('The todo list is empty!');
-  //   } else {
-  //     console.log('My Todos:');
-  //     for (var i = 0; i < this.todos.length; i++) {
-  //       if (this.todos[i].completed === true) {
-  //           console.log('(o)', this.todos[i].todoText);
-  //       } else {
-  //           console.log('(x)', this.todos[i].todoText);
-  //       }
-  //     }
-  //   }
-  // },
   addTodo: function(todoText) {
     this.todos.push({
       todoText: todoText,
@@ -34,20 +20,19 @@ var todoList = {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
 
-    // get the number of completed todos
+    // Get number of completed todos.
     for (var i = 0; i < totalTodos; i++) {
       if (this.todos[i].completed === true) {
         completedTodos++;
       }
     }
 
-    // if everthing is true make everything false
+    // Case 1: If everything’s true, make everything false.
     if (completedTodos === totalTodos) {
-      //make everything false
       for (var i = 0; i < totalTodos; i++) {
         this.todos[i].completed = false;
       }
-      //otherwise make everything true
+    // Case 2: Otherwise, make everything true.
     } else {
       for (var i = 0; i < totalTodos; i++) {
         this.todos[i].completed = true;
@@ -91,21 +76,35 @@ var handlers = {
 
 var view = {
   displayTodos: function() {
-    var todoUl = document.querySelector('ul');
-    todoUl.innerHTML = '';
+    var todosUl = document.querySelector('ul');
+    todosUl.innerHTML = '';
     for (var i = 0; i < todoList.todos.length; i++) {
       var todoLi = document.createElement('li');
       var todo = todoList.todos[i];
       var todoTextWithCompletion = '';
 
       if (todo.completed === true) {
-        todoTextWithCompletion = '(o) ' + todo.todoText;
-      } else {
         todoTextWithCompletion = '(x) ' + todo.todoText;
+      } else {
+        todoTextWithCompletion = '( ) ' + todo.todoText;
       }
 
+      todoLi.id = i;
       todoLi.textContent = todoTextWithCompletion;
-      todoUl.appendChild(todoLi);
+      todoLi.appendChild(this.createDeleteButton());
+      todosUl.appendChild(todoLi);
     }
+  },
+  createDeleteButton: function() {
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteButton';
+    return deleteButton;
   }
 };
+
+var todosUl = document.querySelector('ul');
+
+todosUl.addEventListener('click', function(event) {
+  console.log(event.target.parentNode.id);
+});
